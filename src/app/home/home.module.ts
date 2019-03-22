4c259f2563b9ded67b92dc69a2b6083a1626a5a1
freekieh <freekiehsoes@gmail.com>
2019-03-22 13:34:09 +0100

    Created data service

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
+import { UpperPipe } from '../pipes/upper.pipe';
 
 @NgModule({
   imports: [
@@ -18,6 +19,6 @@ import { HomePage } from './home.page';
       }
     ])
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, UpperPipe]
 })
 export class HomePageModule {}
