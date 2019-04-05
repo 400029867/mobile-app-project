87bb2d2190a36167d120f46a73e3f6b8dbf6b078
freekieh <freekiehsoes@gmail.com>
2019-04-05 09:49:54 +0200

    Setup firebase done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,30 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { environment } from 'src/environments/environment';
+
+//angular firebase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+
+// AngularFireAuthModule
+// AngularFireDatabaseModule
+// AngularFireFunctionsModule
+// AngularFirestoreModule
+// AngularFireStorageModule
+// AngularFireMessagingModule
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
